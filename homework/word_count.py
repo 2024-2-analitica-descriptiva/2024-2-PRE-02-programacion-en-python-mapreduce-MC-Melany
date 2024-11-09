8c0edda3c7f8ff9885ab0d689b5b9418a402c29f
Melany Castaño Rios <mecastanor@unal.edu.co>
2024-11-08 20:19:56 -0500

    Ejercicio 2 -Clase 2

--- homework/word_count.py
+++ homework/word_count.py
@@ -1,7 +1,7 @@
 """Taller evaluable"""
 
 # pylint: disable=broad-exception-raised
-
+import string
 import fileinput
 import glob
 import os.path
@@ -25,9 +25,17 @@ from itertools import groupby
 #
 def load_input(input_directory):
     """Funcion load_input"""
+    
+    files = glob.glob(f"{input_directory}/*")
+    
+    sequence = []
+    with fileinput.input(files=files) as f: # lee un archivo y despues le secuencialmente los archivos 
+        for line in f: 
+            sequence.append(( fileinput.filename (), line)) #
+            
+        
+    return sequence 
 
-
-#
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). Esta función
 # realiza el preprocesamiento de las líneas de texto,
@@ -35,7 +43,22 @@ def load_input(input_directory):
 def line_preprocessing(sequence):
     """Line Preprocessing"""
 
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation))) #agregar 
+        for key, value in sequence
+    ]    
 
+    sequence = [
+        (key, value.lower())
+        for key, value in sequence
+    ]
+
+    sequence = [
+        (key, value.strip())
+        for key, value in sequence
+    ]
+    return sequence
+     
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). En este caso,
@@ -51,6 +74,8 @@ def line_preprocessing(sequence):
 def mapper(sequence):
     """Mapper"""
 
+    # como contar cuantas palabras por texto en grandes volumenes de datos 
+    return [(word, 1) for _, text_line in sequence for word in text_line.split()]
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -65,7 +90,8 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
-
+#    
+    return sorted(sequence, key=lambda x: x[0])
 
 #
 # Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
@@ -75,16 +101,28 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
-
+  # devolver valores en duplas de listas, si iba 1 en cada uno y hay 3 diferentes mostrara ya los 3 
+    diccionario = {}
+    for key, value in sequence:
+        if key in diccionario:
+            diccionario[key] += value
+        else:
+            diccionario[key] = value
+    return list(diccionario.items())
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de
 # directorio y lo crea. Si el directorio existe, lo borra
 #
+
+
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
-
-
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 #
 # Escriba la función save_output, la cual almacena en un archivo de texto
 # llamado part-00000 el resultado del reducer. El archivo debe ser guardado en
@@ -95,7 +133,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
-
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 #
 # La siguiente función crea un archivo llamado _SUCCESS en el directorio
@@ -103,6 +143,8 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
@@ -110,10 +152,24 @@ def create_marker(output_directory):
 #
 def run_job(input_directory, output_directory):
     """Job"""
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+
+    from pprint import pprint
+    print()
+    pprint(sequence)
+    print()
 
 
 if __name__ == "__main__":
     run_job(
-        "input",
-        "output",
+        "files/input",
+        "files/output",
     )
